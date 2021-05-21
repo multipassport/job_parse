25297d052aa5fa43796f7d518da9aa72d78fe2e2
multipassport <mashross@gmail.com>
2021-05-21 14:58:34 +0300

    Use appropriate variables names

--- job_parse.py
+++ job_parse.py
@@ -8,7 +8,7 @@ from math import isnan
 from terminaltables import AsciiTable
 
 
-def predict_rub_salary_hh(vacancy):
+def get_salary_range_hh(vacancy):
     if vacancy['salary']['currency'] != 'RUR':
         return None, None
     minimal_salary = vacancy['salary']['from']
@@ -16,7 +16,7 @@ def predict_rub_salary_hh(vacancy):
     return minimal_salary, maximal_salary
 
 
-def predict_rub_salary_sj(vacancy):
+def get_salary_range_sj(vacancy):
     if (minimal_salary := vacancy['payment_from']) == 0:
         minimal_salary = None
     if (maximal_salary := vacancy['payment_to']) == 0:
@@ -48,10 +48,10 @@ def fetch_vacancies_hh(url, language):
             }
         response = requests.get(url, params=payload)
         response.raise_for_status()
-        page_data = response.json()
-        if page >= page_data['pages']:
+        vacancies_on_page = response.json()
+        if page >= vacancies_on_page['pages']:
             break
-        yield from page_data['items']
+        yield from vacancies_on_page['items']
 
 
 def fetch_vacancies_sj(url, language):
@@ -67,44 +67,44 @@ def fetch_vacancies_sj(url, language):
             }
         response = requests.get(url, params=payload, headers=header)
         response.raise_for_status()
-        page_data = response.json()
-        if page >= page_data['total']:
+        vacancies_on_page = response.json()
+        if page >= vacancies_on_page['total']:
             break
-        yield from page_data['objects']
+        yield from vacancies_on_page['objects']
 
 
-def get_language_data(
-        vacancy_salaries, total_vacancies, languages_info, language
+def get_language_vacancies(
+        vacancy_salaries, total_vacancies, language_vacancies, language
         ):
     mean_salary = numpy.nanmean(numpy.array(vacancy_salaries, dtype=float))
 
     vacancies_processed = sum(1 for salary in vacancy_salaries if salary)
-    languages_info[language]['vacancies_found'] = total_vacancies
-    languages_info[language]['vacancies_processed'] = vacancies_processed
-    languages_info[language]['average_salary'] = None
+    language_vacancies[language]['vacancies_found'] = total_vacancies
+    language_vacancies[language]['vacancies_processed'] = vacancies_processed
+    language_vacancies[language]['average_salary'] = None
     if not isnan(mean_salary):
-        languages_info[language]['average_salary'] = int(mean_salary)
-    return languages_info
+        language_vacancies[language]['average_salary'] = int(mean_salary)
+    return language_vacancies
 
 
-def draw_table(vacancies_data, title):
+def draw_table(vacancies, title):
     header_row = [
         'Язык программирования',
         'Вакансий найдено',
         'Вакансий обработано',
         'Средняя зарплата'
         ]
-    table_data = [header_row]
+    vacancies_table = [header_row]
 
-    for language, language_data in vacancies_data.items():
+    for language, language_vacancy in vacancies.items():
         table_row = [
             language,
-            language_data['vacancies_found'],
-            language_data['vacancies_processed'],
-            language_data['average_salary']]
-        table_data.append(table_row)
+            language_vacancy['vacancies_found'],
+            language_vacancy['vacancies_processed'],
+            language_vacancy['average_salary']]
+        vacancies_table.append(table_row)
 
-    table = AsciiTable(table_data, title=title)
+    table = AsciiTable(vacancies_table, title=title)
     print(table.table)
 
 
@@ -113,8 +113,8 @@ if __name__ == '__main__':
     hh_url = 'https://api.hh.ru/vacancies'
     sj_url = 'https://api.superjob.ru/2.33/vacancies/'
 
-    hh_languages_info = {}
-    sj_languages_info = {}
+    hh_vacancies_for_language = {}
+    sj_vacancies_for_language = {}
     languages = [
         'Python',
         'Java',
@@ -140,31 +140,31 @@ if __name__ == '__main__':
         ]
 
     for language in languages:
-        sj_languages_info[language] = {}
+        sj_vacancies_for_language[language] = {}
         sj_vacancy_salaries = []
         sj_vacancies = fetch_vacancies_sj(sj_url, language)
 
         for vacancy in sj_vacancies:
-            salary_from, salary_to = predict_rub_salary_sj(vacancy)
+            salary_from, salary_to = get_salary_range_sj(vacancy)
             salary = predict_salary(salary_from, salary_to)
             sj_vacancy_salaries.append(salary)
         sj_total_vacancies = len(sj_vacancy_salaries)
-        get_language_data(
-            sj_vacancy_salaries, sj_total_vacancies, sj_languages_info, language
+        get_language_vacancies(
+            sj_vacancy_salaries, sj_total_vacancies, sj_vacancies_for_language, language
             )
 
-        hh_languages_info[language] = {}
+        hh_vacancies_for_language[language] = {}
         hh_vacancy_salaries = []
         hh_vacancies = fetch_vacancies_hh(hh_url, language)
 
         for vacancy in hh_vacancies:
-            salary_from, salary_to = predict_rub_salary_hh(vacancy)
+            salary_from, salary_to = get_salary_range_hh(vacancy)
             salary = predict_salary(salary_from, salary_to)
             hh_vacancy_salaries.append(salary)
         hh_total_vacancies = len(hh_vacancy_salaries)
-        get_language_data(
-            hh_vacancy_salaries, hh_total_vacancies, hh_languages_info, language
+        get_language_vacancies(
+            hh_vacancy_salaries, hh_total_vacancies, hh_vacancies_for_language, language
             )
 
-    draw_table(hh_languages_info, 'HeadHunters')
-    draw_table(sj_languages_info, 'SuperJob')
+    draw_table(hh_vacancies_for_language, 'HeadHunters')
+    draw_table(sj_vacancies_for_language, 'SuperJob')
