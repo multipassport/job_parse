0493010afb1f0243bed03d2e83b4fb875cc9ca85
multipassport <mashross@gmail.com>
2021-05-25 22:36:09 +0300

    Replace variables names with more appropriate ones

--- job_parse.py
+++ job_parse.py
@@ -30,7 +30,7 @@ def predict_salary(salary_from, salary_to):
     if not salary_from and not salary_to:
         return None
     elif salary_from and salary_to:
-        mean_salary = (salary_from + salary_to)/2
+        mean_salary = (salary_from + salary_to) / 2
     elif not salary_from:
         mean_salary = salary_to * salary_to_coefficient
     elif not salary_to:
@@ -48,7 +48,7 @@ def fetch_vacancies_hh(url, language):
         'period': days_to_parse,
         'only_with_salary': True,
         'per_page': vacancies_per_page
-        }
+    }
     for page in count():
         payload['page'] = page
         response = requests.get(url, params=payload)
@@ -68,7 +68,7 @@ def fetch_vacancies_sj(url, language, token):
         'town': 'Москва',
         'currency': 'rub',
         'count': vacancies_per_page,
-        }
+    }
     for page in count():
         payload['page'] = page
         response = requests.get(url, params=payload, headers=header)
@@ -79,81 +79,68 @@ def fetch_vacancies_sj(url, language, token):
             break
 
 
-def get_language_vacancies(vacancy_salaries, total_vacancies, language):
+def get_table_content(vacancy_salaries, total_vacancies, language):
     mean_salary = numpy.nanmean(numpy.array(vacancy_salaries, dtype=float))
 
     vacancies_processed = sum(1 for salary in vacancy_salaries if salary)
-    language_vacancies = {
+    table_content = {
         'vacancies_found': total_vacancies,
         'vacancies_processed': vacancies_processed,
         'average_salary': None
     }
     if not isnan(mean_salary):
-        language_vacancies['average_salary'] = int(mean_salary)
-    return language_vacancies
+        table_content['average_salary'] = int(mean_salary)
+    return table_content
 
 
-def get_table(vacancies, title):
+def draw_table(table_content, title):
     header_row = [
         'Язык программирования',
         'Вакансий найдено',
         'Вакансий обработано',
         'Средняя зарплата'
-        ]
+    ]
     vacancies_table = [header_row]
 
-    for language, language_vacancy in vacancies.items():
+    for language, language_vacancy in table_content.items():
         table_row = [
             language,
             language_vacancy['vacancies_found'],
             language_vacancy['vacancies_processed'],
-            language_vacancy['average_salary']]
+            language_vacancy['average_salary']
+        ]
         vacancies_table.append(table_row)
 
     table = AsciiTable(vacancies_table, title=title)
     return table.table
 
 
-def get_sj_vacancies(
-        fetch_vacancies_sj,
-        get_salary_range_sj,
-        predict_salary,
-        get_language_vacancies,
-        language
-        ):
-    sj_vacancy_salaries = []
-    sj_vacancies = fetch_vacancies_sj(sj_url, language, sj_token)
+def get_sj_table_content(language):
+    vacancy_salaries = []
+    vacancies = fetch_vacancies_sj(sj_url, language, sj_token)
 
-    for vacancy in sj_vacancies:
+    for vacancy in vacancies:
         salary_from, salary_to = get_salary_range_sj(vacancy)
         salary = predict_salary(salary_from, salary_to)
-        sj_vacancy_salaries.append(salary)
-    sj_total_vacancies = len(sj_vacancy_salaries)
-    sj_language_vacancies = get_language_vacancies(
-        sj_vacancy_salaries, sj_total_vacancies, language
-        )
-    return sj_language_vacancies
+        vacancy_salaries.append(salary)
+    total_vacancies = len(vacancy_salaries)
+    table_content = get_table_content(
+        vacancy_salaries, total_vacancies, language)
+    return table_content
 
 
-def get_hh_vacancies(
-        fetch_vacancies_hh,
-        get_salary_range_hh,
-        predict_salary,
-        get_language_vacancies,
-        language
-        ):
-    hh_vacancy_salaries = []
-    hh_vacancies = fetch_vacancies_hh(hh_url, language)
+def get_hh_table_content(language):
+    vacancy_salaries = []
+    vacancies = fetch_vacancies_hh(hh_url, language)
 
-    for vacancy in hh_vacancies:
+    for vacancy in vacancies:
         salary_from, salary_to = get_salary_range_hh(vacancy)
         salary = predict_salary(salary_from, salary_to)
-        hh_vacancy_salaries.append(salary)
-    hh_total_vacancies = len(hh_vacancy_salaries)
-    hh_language_vacancies = get_language_vacancies(
-        hh_vacancy_salaries, hh_total_vacancies, language
-        )
-    return hh_language_vacancies
+        vacancy_salaries.append(salary)
+    total_vacancies = len(vacancy_salaries)
+    table_content = get_table_content(
+        vacancy_salaries, total_vacancies, language)
+    return table_content
 
 
 if __name__ == '__main__':
@@ -163,53 +150,41 @@ if __name__ == '__main__':
     hh_url = 'https://api.hh.ru/vacancies'
     sj_url = 'https://api.superjob.ru/2.33/vacancies/'
 
-    hh_vacancies_for_language = {}
-    sj_vacancies_for_language = {}
+    hh_table_content = {}
+    sj_table_content = {}
     languages = [
         'Python',
         'Java',
-        'JavaScript',
-        'C#',
-        'PHP',
-        'C',
-        'C++',
-        'R',
-        'Objective-C',
-        'Swift',
-        'TypeScript',
-        'Matlab',
-        'Kotlin',
-        'Go',
-        'Ruby',
-        'VBA',
-        'RUST',
-        'Scala',
-        'Visual Basic',
-        'Ada',
-        'Lua',
+        # 'JavaScript',
+        # 'C#',
+        # 'PHP',
+        # 'C',
+        # 'C++',
+        # 'R',
+        # 'Objective-C',
+        # 'Swift',
+        # 'TypeScript',
+        # 'Matlab',
+        # 'Kotlin',
+        # 'Go',
+        # 'Ruby',
+        # 'VBA',
+        # 'RUST',
+        # 'Scala',
+        # 'Visual Basic',
+        # 'Ada',
+        # 'Lua',
         ]
 
     for language in languages:
         try:
-            sj_vacancies_for_language[language] = get_sj_vacancies(
-                fetch_vacancies_sj,
-                get_salary_range_sj,
-                predict_salary,
-                get_language_vacancies,
-                language
-                )
+            sj_table_content[language] = get_sj_table_content(language)
         except (HTTPError, ConnectionError) as error:
             logging.exception(error)
         try:
-            hh_vacancies_for_language[language] = get_hh_vacancies(
-                fetch_vacancies_hh,
-                get_salary_range_hh,
-                predict_salary,
-                get_language_vacancies,
-                language
-                )
+            hh_table_content[language] = get_hh_table_content(language)
         except (HTTPError, ConnectionError) as error:
             logging.exception(error)
 
-    print(get_table(hh_vacancies_for_language, 'HeadHunters'))
-    print(get_table(sj_vacancies_for_language, 'SuperJob'))
+    print(draw_table(hh_table_content, 'HeadHunters'))
+    print(draw_table(sj_table_content, 'SuperJob'))
