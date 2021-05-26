6454cf2d6109965cbfc59152094fbf22e6eac70b
multipassport <mashross@gmail.com>
2021-05-26 19:26:13 +0300

    Place variables in corresponding function callers

--- job_parse.py
+++ job_parse.py
@@ -22,7 +22,7 @@ def predict_salary(salary_from, salary_to):
     salary_from_coefficient = 1.2
 
     if not salary_from and not salary_to:
-        return None
+        mean_salary = None
     elif salary_from and salary_to:
         mean_salary = (salary_from + salary_to) / 2
     elif salary_from:
@@ -32,17 +32,7 @@ def predict_salary(salary_from, salary_to):
     return mean_salary
 
 
-def fetch_vacancies_hh(url, language):
-    moscow_id = 1
-    days_to_parse = 30
-    vacancies_per_page = 100
-    payload = {
-        'text': f'программист {language}',
-        'area': moscow_id,
-        'period': days_to_parse,
-        'only_with_salary': True,
-        'per_page': vacancies_per_page
-    }
+def fetch_vacancies_hh(url, payload):
     for page in count():
         payload['page'] = page
         response = requests.get(url, params=payload)
@@ -53,16 +43,7 @@ def fetch_vacancies_hh(url, language):
             break
 
 
-def fetch_vacancies_sj(url, language, token):
-    vacancies_per_page = 100
-    header = {'X-Api-App-Id': token}
-    payload = {
-        'keyword': f'Программист {language}',
-        'catalogues': 'Разработка, программирование',
-        'town': 'Москва',
-        'currency': 'rub',
-        'count': vacancies_per_page,
-    }
+def fetch_vacancies_sj(url, header, payload):
     for page in count():
         payload['page'] = page
         response = requests.get(url, params=payload, headers=header)
@@ -109,57 +90,67 @@ def draw_table(table_content, title):
     return table.table
 
 
-def get_sj_table_content(language):
-    vacancy_salaries = []
-    vacancies = fetch_vacancies_sj(sj_url, language, sj_token)
+def get_sj_table_content(language, token):
+    url = 'https://api.superjob.ru/2.33/vacancies/'
 
-    for vacancy in vacancies:
-        minimal_salary = vacancy['payment_from']
-        maximal_salary = vacancy['payment_to']
-        if salary := predict_salary(minimal_salary, maximal_salary):
-            vacancy_salaries.append(salary)
-    total_vacancies = fetch_sj_vacancies_number(sj_url, language, sj_token)
-    table_content = get_table_content(
-        vacancy_salaries, total_vacancies, language)
-    return table_content
-
-
-def get_hh_table_content(language):
-    vacancy_salaries = []
-    vacancies = fetch_vacancies_hh(hh_url, language)
-
-    for vacancy in vacancies:
-        salary_from, salary_to = get_salary_range_hh(vacancy)
-        if salary := predict_salary(salary_from, salary_to):
-            vacancy_salaries.append(salary)
-    total_vacancies = fetch_hh_vacancies_number(hh_url, language)
-    table_content = get_table_content(
-        vacancy_salaries, total_vacancies, language)
-    return table_content
-
-
-def fetch_hh_vacancies_number(url, language):
-    moscow_id = 1
-    days_to_parse = 30
-    payload = {
-        'text': f'программист {language}',
-        'area': moscow_id,
-        'period': days_to_parse,
-        'only_with_salary': True,
-    }
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response.json()['found']
-
-
-def fetch_sj_vacancies_number(url, language, token):
+    vacancies_per_page = 100
     header = {'X-Api-App-Id': token}
     payload = {
         'keyword': f'Программист {language}',
         'catalogues': 'Разработка, программирование',
         'town': 'Москва',
         'currency': 'rub',
+        'count': vacancies_per_page,
     }
+
+    vacancy_salaries = []
+    vacancies = fetch_vacancies_sj(url, header, payload)
+
+    for vacancy in vacancies:
+        minimal_salary = vacancy['payment_from']
+        maximal_salary = vacancy['payment_to']
+        if salary := predict_salary(minimal_salary, maximal_salary):
+            vacancy_salaries.append(salary)
+    total_vacancies = fetch_sj_vacancies_number(url, header, payload)
+    table_content = get_table_content(
+        vacancy_salaries, total_vacancies, language)
+    return table_content
+
+
+def get_hh_table_content(language):
+    url = 'https://api.hh.ru/vacancies'
+
+    moscow_id = 1
+    days_to_parse = 30
+    vacancies_per_page = 100
+    payload = {
+        'text': f'программист {language}',
+        'area': moscow_id,
+        'period': days_to_parse,
+        'only_with_salary': True,
+        'per_page': vacancies_per_page
+    }
+
+    vacancy_salaries = []
+    vacancies = fetch_vacancies_hh(url, payload)
+
+    for vacancy in vacancies:
+        salary_from, salary_to = get_salary_range_hh(vacancy)
+        if salary := predict_salary(salary_from, salary_to):
+            vacancy_salaries.append(salary)
+    total_vacancies = fetch_hh_vacancies_number(url, payload)
+    table_content = get_table_content(
+        vacancy_salaries, total_vacancies, language)
+    return table_content
+
+
+def fetch_hh_vacancies_number(url, payload):
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()['found']
+
+
+def fetch_sj_vacancies_number(url, header, payload):
     response = requests.get(url, params=payload, headers=header)
     response.raise_for_status()
     return response.json()['total']
@@ -169,8 +160,6 @@ if __name__ == '__main__':
     logging.basicConfig(filename='job_parse.log', filemode='w')
     load_dotenv()
     sj_token = os.getenv('SUPERJOB_TOKEN')
-    hh_url = 'https://api.hh.ru/vacancies'
-    sj_url = 'https://api.superjob.ru/2.33/vacancies/'
 
     hh_table_content = {}
     sj_table_content = {}
@@ -196,11 +185,11 @@ if __name__ == '__main__':
         'Visual Basic',
         'Ada',
         'Lua',
-        ]
+    ]
 
     for language in languages:
         try:
-            sj_table_content[language] = get_sj_table_content(language)
+            sj_table_content[language] = get_sj_table_content(language, sj_token)
         except (HTTPError, ConnectionError) as error:
             logging.exception(error)
         try:
